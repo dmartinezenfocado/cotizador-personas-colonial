0e9db296e3a03b76fe40484fe3b1d43976623e29
Talia Villaman <talia02091955@gmail.com>
2025-10-14 11:57:02 -0400

    Modificacion de version angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-  HTTP_INTERCEPTORS,
-  withFetch,
-} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS, withFetch } from '@angular/common/http';
 //import { httpInterceptorProviders } from './core/data/interceptors';
 
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
